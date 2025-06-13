6170be59095f27b9d288e5d837f5abbe3f322f54
light <balathan2004@gmail.com>
2025-06-13 17:54:01 +0530

    fixed src no-restart bug

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,39 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { print } from "./src/utils/logger";
+import { print } from "./utils/logger";
 global.print = print;
 import express, { Request, Response } from "express";
 import cors from "cors";
-import apiRoute from "./src/routes/api";
-import authRouter from "./src/routes/auth";
+import apiRoute from "./routes/api";
+import authRouter from "./routes/auth";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin:"*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+  <html>
+    <head><title>Test</title></head>
+    <body>
+      <h1>Hello from Express</h1>
+      <p>This is a simple HTML response.</p>
+    </body>
+  </html>
+`);
+});
+
 app.use("/api", apiRoute);
 
 app.use("/auth", authRouter);
-
-app.get("/hello", (req: Request, res: Response) => {
-  res.send(`
-    <html>
-      <head><title>Test</title></head>
-      <body>
-        <h1>Hello from Express</h1>
-        <p>This is a simple HTML response.</p>
-      </body>
-    </html>
-  `);
-});
 
 app.get("/test", (req: Request, res: Response) => {
   res.json({ message: "test route", status: 200 });
@@ -40,5 +43,4 @@ app.listen(port, () => {
   print("server listening");
 });
 
-
-module.exports=app
+module.exports = app;
